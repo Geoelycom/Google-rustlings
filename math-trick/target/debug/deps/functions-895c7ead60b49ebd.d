a2c8b7af832804a12f3b6a6edbf6f31f1f46121a
Elyan <ekenimohelyanpheta@gmail.com>
2023-01-10 08:15:47 -0800

    fizzbuzz challenge

---
+++ math-trick/target/debug/deps/functions-895c7ead60b49ebd.d
@@ -0,0 +1,5 @@
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/deps/functions-895c7ead60b49ebd: src/bin/functions.rs
+
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/deps/functions-895c7ead60b49ebd.d: src/bin/functions.rs
+
+src/bin/functions.rs:
