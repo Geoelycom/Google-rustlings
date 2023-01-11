84c47a2e04fedc21f3493ce32722779c70a8efc8
Elyan <ekenimohelyanpheta@gmail.com>
2023-01-11 10:51:15 -0800

    solvd methods task

---
+++ math-trick/target/debug/deps/methods-411e1b4c80b10fb0.d
@@ -0,0 +1,5 @@
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/deps/methods-411e1b4c80b10fb0: src/bin/methods.rs
+
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/deps/methods-411e1b4c80b10fb0.d: src/bin/methods.rs
+
+src/bin/methods.rs:
---
+++ math-trick/target/debug/methods.d
@@ -0,0 +1,1 @@
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/methods: /Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/src/bin/methods.rs
