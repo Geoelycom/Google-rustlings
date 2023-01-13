e7c0878ea3e14f411fe08f1358d9d11883c12b3c
Elyan <ekenimohelyanpheta@gmail.com>
2023-01-13 08:52:26 -0800

    solution to implicit conversion

---
+++ math-trick/target/debug/implicit_coversions.d
@@ -0,0 +1,1 @@
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/implicit_coversions: /Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/src/bin/implicit_coversions.rs
