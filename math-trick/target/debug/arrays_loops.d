f296836150e8657984ee2111fc808ba20cd2145d
Elyan <ekenimohelyanpheta@gmail.com>
2023-01-18 15:01:54 -0800

    transpose

---
+++ math-trick/target/debug/arrays_loops.d
@@ -0,0 +1,1 @@
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/arrays_loops: /Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/src/bin/arrays_loops.rs
---
+++ math-trick/target/debug/deps/tranpose_matrix-1a136425a7a57655.d
@@ -0,0 +1,5 @@
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/deps/tranpose_matrix-1a136425a7a57655: src/bin/tranpose_matrix.rs
+
+/Users/elyanekenimoh/hello-rust/Google-rustlings/math-trick/target/debug/deps/tranpose_matrix-1a136425a7a57655.d: src/bin/tranpose_matrix.rs
+
+src/bin/tranpose_matrix.rs:
