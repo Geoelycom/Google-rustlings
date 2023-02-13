a9404a3644589ac1c8c40d401e6022cf163ffe12
Elyan <ekenimohelyanpheta@gmail.com>
2023-02-13 12:05:12 -0800

    ok result commit

--- math-trick/src/bin/result.rs
+++ math-trick/src/bin/result.rs
@@ -43,3 +43,46 @@ fn main() {
   }
   //println!("choice = {:?}", choice);
 }
+
+struct Adult {
+  age: u8,
+  name: String, 
+}
+
+impl Adult {
+  fn Adult_struct(age: u8, &str) -> Result<Self, &str> {
+    if age >= 21 {
+      Ok(Self {
+        age,
+        name: name.to_string(),
+      })
+    } else {
+      Err("age must be at least 21")
+    }
+ }
+}
+
+
+fn return_result() {
+  let child = Adult::new(12, "anita");
+  let adult = Adult::new(22, "john");
+
+  match child {
+    Ok(child) => println!("{} is {} years old", child.name, child.age),
+    Err(e) => println("{e}"),
+  }
+
+  match adult {
+    Ok(adult) => println!("{} is {} years old", adult.name, adult.age),
+    Err(e) => println("{e}"),
+  }
+  let return_adult: Result<Adult, _> Adult_struct("")
+
+}
+
+
+
+
+
+
+
