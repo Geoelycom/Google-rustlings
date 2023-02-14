31b12e7920fdea2e12fba58aa2f9cb13abd8fb79
Elyan <ekenimohelyanpheta@gmail.com>
2023-02-14 00:49:27 -0800

    learning hashes

---
+++ math-trick/src/bin/hashmaps.rs
@@ -0,0 +1,18 @@
+/* 
+HASH MAP
+.. This is a collection that stores data as key:value pairs
+1. data is located using the "key",
+2. the data is the value 
+.. Similar to objects in javascript and definations in a dictionary
+they are very fast to retrieve data using the key
+*/
+//Example
+use std::collections::HashMap;
+
+
+let mut people = HashMap::new();
+people.insert("susan", 21),
+people.insert("john", 19),
+people.insert("mary", 25),
+people.insert("mike", 11),
+people.insert("sophia", 41),
