f6718beacd5ffaee451aed8e459d3c4d16bc4195
Elyan <ekenimohelyanpheta@gmail.com>
2023-03-13 10:16:43 -0700

    lifetimes

--- math-trick/src/bin/lifetimes.rs
+++ math-trick/src/bin/lifetimes.rs
@@ -4,13 +4,31 @@
 
 // unsafe program
 fn main() {
-  let r;
+  let string1 = String::from("long string is long");
+
   {
-    let x = 5;
-    r = &x;
+      let string2 = String::from("xyz");
+      let result = longest(string1.as_str(), string2.as_str());
+      println!("The longest string is {}", result);
   }
-  println!("r: {}", r);
 }
 
 // The Borrow Checker Ensures Data Outlives Its References
 
+
+fn longest(x: &str, y: &str) -> &str {
+  if x.len() > y.len() {
+      x
+  } else {
+      y
+  }
+}
+
+// rewrite longest function with lifetimes
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+  if x.len() > y.len() {
+      x
+  } else {
+      y
+  }
+}
