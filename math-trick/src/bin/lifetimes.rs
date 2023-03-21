7935dcc7d1b17f74f58f6c1e15fde1ef866b44c3
Elyan <ekenimohelyanpheta@gmail.com>
2023-03-21 10:45:47 -0700

    add ellision

--- math-trick/src/bin/lifetimes.rs
+++ math-trick/src/bin/lifetimes.rs
@@ -48,3 +48,14 @@ fn  main() {
 
 // understanding lifetimes Elision
 
+fn first_word(s: &str) -> &str {
+  let bytes = s.as_bytes();
+
+  for (i, &item) in bytes.iter().enumerate() {
+    if item == 'b' {
+      return &s[0..i];
+    }
+
+    &s[..]
+  }
+}
