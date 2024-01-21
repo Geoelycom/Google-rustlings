375c41710894c9167cc35b7d0f827b87484e93e0
Elyan <ekenimohelyanpheta@gmail.com>
2024-01-21 14:53:00 -0800

    add new modules

--- Rust101/src/bin/http.rs
+++ Rust101/src/bin/http.rs
@@ -46,10 +46,11 @@ mod server {
 
 mod http {
     pub mod request {
+      use super::method::Method;
      pub struct Request {
       path: String,
       query_string: Option<String>,
-      method: super::method::Method,
+      method: Method,
     }
 }
 
