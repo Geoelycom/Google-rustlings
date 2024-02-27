60ecf7b7ce320fdfaddab0a47d2c90fa4feaf386
Elyan <ekenimohelyanpheta@gmail.com>
2024-02-27 14:51:21 -0800

    Add:implement summary trait

--- Rust101/src/bin/traits.rs
+++ Rust101/src/bin/traits.rs
@@ -9,3 +9,36 @@ pub fn notify<T: summary>(item: &T) {
 
 pub fn notify2<T: summary>(item1: &T, item2: &T){}
 //parameters passed into this must be of thesame type in each item1 and item2
+
+
+//Implementing a straight.
+
+pub trait Summary {
+  fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+  pub headline: String,
+  pub location: String,
+  pub author: String,
+  pub content: String,
+}
+
+impl Summary for NewsArticle {
+  fn summarize(&self) -> String {
+      format!("{}, by {} ({})", self.headline, self.author, self.location)
+  }
+}
+
+pub struct Tweet {
+  pub username: String,
+  pub content: String,
+  pub reply: bool,
+  pub retweet: bool,
+}
+
+impl Summary for Tweet {
+  fn summarize(&self) -> String {
+      format!("{}: {}", self.username, self.content)
+  }
+}
