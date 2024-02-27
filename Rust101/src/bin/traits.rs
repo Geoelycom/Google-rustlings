e229c5cff0c4507237af207273b0cd50c94af570
Elyan <ekenimohelyanpheta@gmail.com>
2024-02-27 15:20:31 -0800

    update task for new hire

--- Rust101/src/bin/traits.rs
+++ Rust101/src/bin/traits.rs
@@ -42,3 +42,19 @@ impl Summary for Tweet {
       format!("{}: {}", self.username, self.content)
   }
 }
+
+
+use aggregator::{Summary, Tweet};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
